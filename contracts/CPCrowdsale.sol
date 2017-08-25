9392a80c16201b09925326d2124d2b55183c2e5c
blah <tim.galebach@gmail.com>
2017-08-25 21:08:02 +0000

    capped

--- contracts/CPCrowdsale.sol
+++ contracts/CPCrowdsale.sol
@@ -2,13 +2,15 @@ pragma solidity ^0.4.13;
 
 import './CPToken.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 
 
-contract CPCrowdsale is Crowdsale {
+contract CPCrowdsale is CappedCrowdsale {
 
-  function CPCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet, uint cap) Crowdsale(_startBlock, _endBlock, _rate, _wallet) {
+  function CPCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet, uint _cap)
+    CappedCrowdsale(_cap)
+    Crowdsale(_startBlock, _endBlock, _rate, _wallet)
+  {
   }
 
   function createTokenContract() internal returns (MintableToken) {
