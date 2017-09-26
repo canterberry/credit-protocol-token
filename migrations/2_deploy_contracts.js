5839adc97eb246bf8f5e93254ad4c0559d343353
blah <tim.galebach@gmail.com>
2017-09-26 10:44:25 +0000

    set ropsten vars

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -11,19 +11,25 @@ module.exports = function(deployer, network) {
     const thirtyDays = 30 * 24 * 60 * 60;
 
     if (network == "ropsten") {
-        const wallet = "";
-        var whitelist = "0x25d48524424bea76d51eb2056ca83511a6e58ef7";
-        const startTime = october1st1600;
-        const endTime   = november1st0000;
-        const whitelistEndTime = new web3.BigNumber(startTime + fiveDays);
-        const openWhitelistEndTime = new web3.BigNumber(startTime + sevenDays);
+        var ropstenWhitelistAddress = "0x25d48524424bea76d51eb2056ca83511a6e58ef7";
+        //below are metamask accounts
+        const ropstenWallet = "0x406Dd5315e6B63d6F1bAd0C4ab9Cd8EBA6Bb1bD2";
+        const ropstenAirdropWallet = "0xE241c02E638ce0a1beFEC039FF4551B79F2Cf8D2";
+        const ropstenAdvisorWallet = "0x6C48110d0F02814f5b27aB7dc9734d69494389f4";
+        const ropstenStakingWallet = "0xacA6476054E2c57e8a1359F8a8c63Eb2B47De6B0";
+        const ropstenPrivateSaleWallet = "0xB07cd7De89Fa764301b6cC5f41eCd1497b72a475";
+        const ropstenStartTime = october1st1600;
+        const ropstenEndTime   = november1st0000;
+        const ropstenWhitelistEndTime = new web3.BigNumber(ropstenStartTime + fiveDays);
+        const ropstenOpenWhitelistEndTime = new web3.BigNumber(ropstenStartTime + sevenDays);
+        deployer.deploy(CPCrowdsale, ropstenStartTime, ropstenEndTime, ropstenWhitelistEndTime, ropstenOpenWhitelistEndTime, ropstenWallet, ropstenWhitelistAddress, ropstenAirdropWallet, ropstenAdvisorWallet, ropstenStakingWallet, ropstenPrivateSaleWallet);
     }
 
     else if (network == "mainnet") {
         var whitelistAddress = "0xdaF5520A1BA8D71CDb81C69c72D736dAb058C602";
         const wallet = "";
-        const advisorWallet = "";
         const airdropWallet = "";
+        const advisorWallet = "";
         const stakingWallet = "";
         const privateSaleWallet = "";
 
@@ -31,7 +37,7 @@ module.exports = function(deployer, network) {
         const endTime   = november1st0000;
         const whitelistEndTime = new web3.BigNumber(startTime + fiveDays);
         const openWhitelistEndTime = new web3.BigNumber(startTime + sevenDays);
-        deployer.deploy(CPCrowdsale, startTime, endTime, whitelistEndTime, openWhitelistEndTime, wallet, whitelistAddress, );
+        deployer.deploy(CPCrowdsale, startTime, endTime, whitelistEndTime, openWhitelistEndTime, wallet, whitelistAddress, airdropWallet, advisorWallet, stakingWallet, privateSaleWallet);
     }
 
     else if (network === "console") {
