c7b4b66496778fc6288c977b5a1423c157685000
blah <tim.galebach@gmail.com>
2017-09-28 13:58:27 +0000

    refactor compiles

--- contracts/CPCrowdsale.sol
+++ contracts/CPCrowdsale.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.15;
 
 import './CPToken.sol';
 import './AbstractWhitelist.sol';
+import './AbstractTiers.sol';
 import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 import 'zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 import 'zeppelin-solidity/contracts/crowdsale/FinalizableCrowdsale.sol';
@@ -29,28 +30,14 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
     uint256 public initialPrivateSaleTokens = 36000000 * (10 ** decimals);
     uint256 public initialDevTokens = nonPublicTokens - (initialAirdropTokens + initialAdvisorTokens + initialStakingTokens + initialPrivateSaleTokens);
 
-    uint256[6] public tierAmountCaps =  [ presaleWeiSold
-                                        , presaleWeiSold + 5000 ether
-                                        , presaleWeiSold + 10000 ether
-                                        , presaleWeiSold + 15000 ether
-                                        , presaleWeiSold + 21000 ether
-                                        , cpCap
-                                        ];
-    uint256[6] public tierRates = [ 2000 // tierRates[0] should never be used, but it is accurate
-                                  , 1500 // Tokens are purchased at a rate of 105-150
-                                  , 1350 // per deciEth, depending on purchase tier.
-                                  , 1250 // tierRates[i] is the purchase rate of tier_i
-                                  , 1150
-                                  , 1050
-                                  ];
-
     AbstractWhitelist private aw;
+    AbstractTiers private at;
     mapping (address => bool) private hasPurchased; // has whitelist address purchased already
     uint256 public whitelistEndTime;
     uint256 public maxWhitelistPurchaseWei;
     uint256 public openWhitelistEndTime;
 
-    function CPCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _whitelistEndTime, uint256 _openWhitelistEndTime, address _wallet, address _whitelistContract, address _airdropWallet, address _advisorWallet, address _stakingWallet, address _privateSaleWallet)
+    function CPCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _whitelistEndTime, uint256 _openWhitelistEndTime, address _wallet, address _tiersContract, address _whitelistContract, address _airdropWallet, address _advisorWallet, address _stakingWallet, address _privateSaleWallet)
         CappedCrowdsale(cpCap)
         FinalizableCrowdsale()
         Crowdsale(_startTime, _endTime, dummyRate, _wallet)  // rate is a dummy value; we use tiers instead
@@ -61,7 +48,6 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
         privateSaleWallet = _privateSaleWallet;
 
         token.mint(_wallet, initialDevTokens);
-
         token.mint(airdropWallet, initialAirdropTokens);
         token.mint(advisorWallet, initialAdvisorTokens);
         token.mint(stakingWallet, initialStakingTokens);
@@ -69,6 +55,7 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
 
         aw = AbstractWhitelist(_whitelistContract);
         require (aw.numUsers() > 0);
+        at = AbstractTiers(_tiersContract);
         whitelistEndTime = _whitelistEndTime;
         openWhitelistEndTime = _openWhitelistEndTime;
         weiRaised = presaleWeiSold;
@@ -76,38 +63,6 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
     }
 
     // Public functions
-
-    function tierIndexByWeiAmount(uint256 weiLevel) public constant returns (uint256) {
-        require(weiLevel <= cpCap);
-        for (uint256 i = 0; i < tierAmountCaps.length; i++) {
-            if (weiLevel <= tierAmountCaps[i]) {
-                return i;
-            }
-        }
-    }
-
-    /**
-     * @dev Calculates how many tokens a given amount of wei can buy at
-     * a particular level of weiRaised. Takes into account tiers of purchase
-     * bonus
-     */
-    function calculateTokens(uint256 _amountWei, uint256 _weiRaised) public constant returns (uint256) {
-        uint256 currentTier = tierIndexByWeiAmount(_weiRaised);
-        uint256 startWeiLevel = _weiRaised;
-        uint256 endWeiLevel = _amountWei.add(_weiRaised);
-        uint256 tokens = 0;
-        for (uint256 i = currentTier; i < tierAmountCaps.length; i++) {
-            if (endWeiLevel <= tierAmountCaps[i]) {
-                tokens = tokens.add((endWeiLevel.sub(startWeiLevel)).mul(tierRates[i]));
-                break;
-            } else {
-                tokens = tokens.add((tierAmountCaps[i].sub(startWeiLevel)).mul(tierRates[i]));
-                startWeiLevel = tierAmountCaps[i];
-            }
-        }
-        return tokens;
-    }
-
     function buyTokens(address beneficiary) public payable whenNotPaused {
         uint256 weiAmount = msg.value;
 
@@ -120,7 +75,7 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
 
         hasPurchased[beneficiary] = true;
 
-        uint256 tokens = calculateTokens(weiAmount, weiRaised);
+        uint256 tokens = at.calculateTokens(weiAmount, weiRaised);
         weiRaised = weiRaised.add(weiAmount);
         token.mint(beneficiary, tokens);
         TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
@@ -140,7 +95,7 @@ contract CPCrowdsale is CappedCrowdsale, FinalizableCrowdsale, Pausable {
     function finalization() internal {
         uint256 remainingWei = cap.sub(weiRaised);
         if (remainingWei > 0) {
-            uint256 remainingDevTokens = calculateTokens(remainingWei, weiRaised);
+            uint256 remainingDevTokens = at.calculateTokens(remainingWei, weiRaised);
             token.mint(wallet, remainingDevTokens);
         }
         CPToken(token).endSale();
---
+++ contracts/Tiers.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.4.15;
+
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+contract Tiers {
+  using SafeMath for uint256;
+
+  uint256 public cpCap = 45000 ether;
+  uint256 public presaleWeiSold = 18000 ether;
+
+  uint256[6] public tierAmountCaps =  [ presaleWeiSold
+                                        , presaleWeiSold + 5000 ether
+                                        , presaleWeiSold + 10000 ether
+                                        , presaleWeiSold + 15000 ether
+                                        , presaleWeiSold + 21000 ether
+                                        , cpCap
+                                        ];
+  uint256[6] public tierRates = [ 2000 // tierRates[0] should never be used, but it is accurate
+                                  , 1500 // Tokens are purchased at a rate of 105-150
+                                  , 1350 // per deciEth, depending on purchase tier.
+                                  , 1250 // tierRates[i] is the purchase rate of tier_i
+                                  , 1150
+                                  , 1050
+                                  ];
+
+  function tierIndexByWeiAmount(uint256 weiLevel) public constant returns (uint256) {
+    require(weiLevel <= cpCap);
+    for (uint256 i = 0; i < tierAmountCaps.length; i++) {
+      if (weiLevel <= tierAmountCaps[i]) {
+        return i;
+      }
+    }
+  }
+
+    /**
+     * @dev Calculates how many tokens a given amount of wei can buy at
+     * a particular level of weiRaised. Takes into account tiers of purchase
+     * bonus
+     */
+    function calculateTokens(uint256 _amountWei, uint256 _weiRaised) public constant returns (uint256) {
+        uint256 currentTier = tierIndexByWeiAmount(_weiRaised);
+        uint256 startWeiLevel = _weiRaised;
+        uint256 endWeiLevel = _amountWei.add(_weiRaised);
+        uint256 tokens = 0;
+        for (uint256 i = currentTier; i < tierAmountCaps.length; i++) {
+            if (endWeiLevel <= tierAmountCaps[i]) {
+                tokens = tokens.add((endWeiLevel.sub(startWeiLevel)).mul(tierRates[i]));
+                break;
+            } else {
+                tokens = tokens.add((tierAmountCaps[i].sub(startWeiLevel)).mul(tierRates[i]));
+                startWeiLevel = tierAmountCaps[i];
+            }
+        }
+        return tokens;
+    }
+
+
+}
